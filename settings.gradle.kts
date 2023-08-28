094f754986c4d0c54ca96ecf1e772f8c2e9d0c94
Clement <clembou00@gmail.com>
2023-08-28 16:54:45 +0200

    Salam alle coum

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "puissance 4 new"
+include(":app")
